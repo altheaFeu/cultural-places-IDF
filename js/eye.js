f21178e275841c8c6ba210d953f125e4f445054c
Althéa Feuillet <123597411+altheaFeu@users.noreply.github.com>
2023-09-16 09:58:18 +0200

    Add files via upload

---
+++ js/eye.js
@@ -0,0 +1,10 @@
+// Permet de cacher ou d'afficher un mot de passe
+
+const passwordInput = document.querySelector("#password")
+const eye = document.querySelector("#eye")
+
+eye.addEventListener("click", function(){
+    this.classList.toggle("fa-eye-slash")
+    const type = passwordInput.getAttribute("type") === "password" ? "text" : "password"
+    passwordInput.setAttribute("type", type)
+})
